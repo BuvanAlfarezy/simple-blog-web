a0302f601e7f66effcce6f0196b95400c526830e
Buvan Alfarezy <buvanalfarezy2305@gmail.com>
2026-04-16 15:22:46 +0700

    code file test config

---
+++ test/Config/DatabaseTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Advan\BlogWeb\Config;
+
+use PHPUnit\Framework\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    public function testGetConnection()
+    {
+        $conn = Database::getConnection();
+        self::assertNotNull($conn);
+    }
+
+    public function testGetConnectionSingleton()
+    {
+        $conn1 = Database::getConnection();
+        $conn2 = Database::getConnection();
+        self::assertSame($conn1, $conn2);
+    }
+
+
+}
