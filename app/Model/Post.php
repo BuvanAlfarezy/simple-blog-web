5d700ca4e977a86b7cbcee44473eaf44a837dc3f
Buvan Alfarezy <buvanalfarezy2305@gmail.com>
2026-04-16 20:01:22 +0700

    kode post model

---
+++ app/Model/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Config\Database;
+
+class Post {
+
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getConnection();
+    }
+
+    public function getAll()
+    {
+        $stmt = $this->db->query("SELECT * FROM posts ORDER BY created_at DESC");
+        return $stmt->fetchAll();
+    }
+
+    public function getById($id)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM posts WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
+}
